2f04a941ab3126b11c779707ee716aa03a93ba0a
Nrupesh Patel <nrupesh.patel2912@gmail.com>
2016-11-11 09:47:41 -0800

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Nrupesh Patel on 11/11/16.
+//  Copyright © 2016 Nrupesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Button Pressed");
+        recordingLabel.text = "Recording Audio"
+    }
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Button Pressed");
+    }
+}
+
